a04ade43d168ab8be0c493aa31d856ce9e2862c3
M Ardian Saputra <putrasaga4@gmail.com>
2023-09-27 14:29:04 +0800

    nyambung database

--- app/src/main/java/project/homesentinel/LoginActivity.kt
+++ app/src/main/java/project/homesentinel/LoginActivity.kt
@@ -2,25 +2,50 @@ package project.homesentinel
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import project.homesentinel.databinding.LoginBinding
 
 class LoginActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
 
-        val btBack : ImageView = findViewById(R.id.l_btBack)
-        val txtRegister: TextView = findViewById(R.id.l_txtRegister)
+    lateinit var binding: LoginBinding
+    lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = LoginBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
 
         //tombol back di login (bagian kiri atas)
-        btBack.setOnClickListener(){
+        binding.lBtBack.setOnClickListener(){
             startActivity(Intent(this, TitleActivity::class.java))
         }
-
-        txtRegister.setOnClickListener(){
+        //tulisan register di bawah
+        binding.lTxtRegister.setOnClickListener(){
             startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        //tombol login
+        binding.lBtlogin.setOnClickListener(){
+            val email = binding.lEmail.text.toString()
+            val password = binding.lPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Data Tidak Boleh Kosong", Toast.LENGTH_SHORT).show()
+            }else{
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(){
+                    if(it.isSuccessful){
+                        startActivity(Intent(this, MenuActivity::class.java))
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
 
     }
--- app/src/main/java/project/homesentinel/RegisterActivity.kt
+++ app/src/main/java/project/homesentinel/RegisterActivity.kt
@@ -6,25 +6,48 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import project.homesentinel.databinding.RegisterBinding
+import android.widget.Toast
 
 class RegisterActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.register)
-        
 
-        val btRegister: Button = findViewById(R.id.r_btRegister)
-        val txtLogin: TextView = findViewById(R.id.r_txtLogin)
-        val btBack: ImageView = findViewById(R.id.r_btBack)
+    lateinit var binding : RegisterBinding
+    lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = RegisterBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
 
         //text login di login (dibagian pling bawah)
-        txtLogin.setOnClickListener(){
+        binding.rTxtLogin.setOnClickListener(){
             startActivity(Intent(this,LoginActivity::class.java))
         }
 
         //tombol back di login (bagian kiri atas)
-        btBack.setOnClickListener(){
+        binding.rBtBack.setOnClickListener(){
             startActivity(Intent(this, TitleActivity::class.java))
+        }
+
+        //tombol register
+        binding.rBtRegister.setOnClickListener(){
+            val name = binding.rName.text.toString()
+            val email = binding.rEmail.text.toString()
+            val password = binding.rPassword.text.toString()
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Data Tidak Boleh Kosong", Toast.LENGTH_SHORT).show()
+            }else{
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(){
+                    if(it.isSuccessful){
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
