6087b9335c1d50cfc7c6c48eb6e2ecf8dadf116a
Rudi_Subriyanto <129402666+Rudi-Subriyanto29@users.noreply.github.com>
2023-10-23 02:58:36 +0800

    Update esp8266.ino

--- IOT_srcHomeSentinel/esp8266.ino
+++ IOT_srcHomeSentinel/esp8266.ino
@@ -1,81 +1,60 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-#define FIREBASE_AUTH "Your secret" 
+#define FIREBASE_AUTH "Your secret"
 #define FIREBASE_HOST "your FIREBASE HOST"
-#define WIFI_SSID " your WIFI SSID"
-#define WIFI_PASSWORD " your WIFI PASSWORD"
+#define WIFI_SSID "your WIFI SSID"
+#define WIFI_PASSWORD "your WIFI PASSWORD"
 
-String values,sensor_data;
+String values, sensor_data;
 
 void setup() {
-  //Initializes the serial connection at 9600 get sensor data from arduino.
-   Serial.begin(9600);
-   
+  // Initializes the serial connection at 9600 to get sensor data from Arduino.
+  Serial.begin(9600);
   delay(1000);
-  
+
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    
   }
-  
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); 
-  
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
+
 void loop() {
+  bool Sr = false;
 
- bool Sr =false;
- 
-  while(Serial.available()){
-    
-        //get sensor data from serial put in sensor_data
-        sensor_data=Serial.readString(); 
-        Sr=true;    
-        
-    }
-  
-delay(1000);
-
-  if(Sr==true){  
-    
-  values=sensor_data;
-  
-  //get comma indexes from values variable
-  int fristCommaIndex = values.indexOf(',');
-  int secondCommaIndex = values.indexOf(',', fristCommaIndex+1);
-  int thirdCommaIndex = values.indexOf(',', secondCommaIndex + 1);
-  
-  //get sensors data from values variable by  spliting by commas and put in to variables  
-  String ultrasonic_value = values.substring(0, fristCommaIndex);
-  String IR_sensor1_value = values.substring(fristCommaIndex+1, secondCommaIndex);
-  String IR_sensor2_value = values.substring(secondCommaIndex+1);
-
-  //store ultrasonic sensor data as string in firebase 
-  Firebase.setString("ultrasonic_value",ultrasonic_value);
-   delay(10);
-  //store IR sensor 1 data as string in firebase 
-  Firebase.setString("IR_sensor1_value",IR_sensor1_value);
-   delay(10);
-  //store IR sensor 2 data as string in firebase 
-  Firebase.setString("IR_sensor2_value",IR_sensor2_value);
-
-  delay(10);
-  
-  //store previous sensors data as string in firebase
-  
-  Firebase.pushString("previous_ultrasonic_value",ultrasonic_value);
-   delay(10);
-  Firebase.pushString("previous_IR_sensor1_value",IR_sensor1_value);
-   delay(10);
-  Firebase.pushString("previous_IR_sensor2_value",IR_sensor2_value);
-  
-  
-  delay(1000);
-  
-  if (Firebase.failed()) {  
-      return;
+  while (Serial.available()) {
+    // Get sensor data from serial and put it in sensor_data.
+    sensor_data = Serial.readString();
+    Sr = true;
   }
-  
-}   
+
+  delay(1000);
+
+  if (Sr == true) {
+    values = sensor_data;
+
+    // Get comma index from the values variable
+    int firstCommaIndex = values.indexOf(',');
+
+    // Get sensor data from values variable by splitting it by commas and store in variables
+    String IR_sensor_value = values.substring(0, firstCommaIndex);
+
+    // Store IR sensor 1 data as a string in Firebase
+    Firebase.setString("IR_sensor_value", IR_sensor_value);
+    delay(10);
+
+    delay(10);
+
+    // Store previous sensors data as a string in Firebase
+    Firebase.pushString("previous_IR_sensor_value", IR_sensor_value);
+    delay(10);
+
+    delay(1000);
+
+    if (Firebase.failed()) {
+      return;
+    }
+  }
 }
